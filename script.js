61056211cb1b160846938a784759b6b20673e440
Manvansh Singh <manvansh086@gmail.com>
2025-04-04 14:50:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add any interactive functionality here
+document.addEventListener('DOMContentLoaded', function() {
+    const registerButtons = document.querySelectorAll('.event button');
+    registerButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            alert('Registration functionality is yet to be implemented!');
+        });
+    });
+});
